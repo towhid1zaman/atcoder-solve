f26008dc40f75e2c1dbeb478a983f9cf9fa70b41
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-04-15 11:17:56 +0600

    Add files via upload

---
+++ abc 55/C.cpp
@@ -0,0 +1,43 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef unsigned long long ll;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+int main(){
+        _ios;
+
+        ll n,m; cin >> n >> m;
+        //for 2
+        m/=2;
+        if(n>=m)cout<<m<<endl;
+        else{
+            ll k = m - n;
+            cout<<m+(k/2)<<endl;
+
+        }
+return 0;
+}
+
