c53785ae87c07a820cde09914426f8d865dc2034
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-06-11 00:03:04 +0600

    Training (beta)

---
+++ Atcoder Boot camp/sumitb2019_c.cpp
@@ -0,0 +1,59 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair< int, int > pii;
+typedef pair< pii, int > ppi;
+#define fill(a,b) memset(a,b,sizeof(a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0;i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(all(v))
+#define maxv(v) *max_element(all(v))
+#define unq(v) sort(all(v)),(v).erase(unique(all(v)),(v).end())
+#define _ ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int inf = 0x3f3f3f3f;// (int)3e18;
+const int maxn = 2000100;
+
+
+
+int main(){
+        _
+        map<int,bool>mp;
+        mp[100]=1;
+        mp[101]=1;
+        mp[102]=1;
+        mp[103]=1;
+        mp[104]=1;
+        mp[105]=1;
+        int tot = 615;
+        int x; cin >> x;
+        for(int i = 106; i<=x; i++){
+            mp[i]|=mp[i-100];
+            mp[i]|=mp[i-101];
+            mp[i]|=mp[i-102];
+            mp[i]|=mp[i-103];
+            mp[i]|=mp[i-104];
+            mp[i]|=mp[i-105];
+        }
+        cout<<mp[x]<<endl;
+
+return 0;
+}
+
+
